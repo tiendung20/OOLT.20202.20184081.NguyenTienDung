e173939b742bf8ce073f641ee4c10c4abfafefe7
= <=>
2021-03-17 00:25:56 +0700

    Update

--- Lab03/MyDate.java
+++ Lab03/MyDate.java
@@ -34,57 +34,61 @@ public class MyDate {
 				c = 1;
 			}
 			switch(date[0]) {
-			case "January": case "Jan.": case "Jan": case "1":
+			case "January": case "Jan.": case "Jan":
 				this.month = 1;
 				kt = 1;
 				break;
-			case "February": case "Feb.": case "Feb": case "2":
+			case "February": case "Feb.": case "Feb":
 				this.month = 2;
 				kt = 1;
 				break;
-			case "March": case "Mar.": case "Mar": case "3":
+			case "March": case "Mar.": case "Mar":
 				this.month = 3;
 				kt = 1;
 				break;
-			case "April": case "Apr.": case "Apr": case "4":
+			case "April": case "Apr.": case "Apr":
 				this.month = 4;
 				kt = 1;
 				break;
-			case "May": case "5":
+			case "May":
 				this.month = 5;
 				kt = 1;
 				break;
-			case "June": case "Jun": case "6":
+			case "June": case "Jun":
 				this.month = 6;
 				kt = 1;
 				break;
-			case "July": case "Jul": case "7":
+			case "July": case "Jul":
 				this.month = 7;
 				kt = 1;
 				break;
-			case "August": case "Aug.": case "Aug": case "8":
+			case "August": case "Aug.": case "Aug":
 				this.month = 8;
 				kt = 1;
 				break;
-			case "September": case "Sept.": case "Sep": case "9":
+			case "September": case "Sept.": case "Sep":
 				this.month = 9;
 				kt = 1;
 				break;
-			case "October": case "Oct.": case "Oct": case "10":
+			case "October": case "Oct.": case "Oct":
 				this.month = 10;
 				kt = 1;
 				break;
-			case "November": case "Nov.": case "Nov": case "11":
+			case "November": case "Nov.": case "Nov":
 				this.month = 11;
 				kt = 1;
 				break;
-			case "December": case "Dec.": case "Dec": case "12":
+			case "December": case "Dec.": case "Dec":
 				this.month = 12;
 				kt = 1;
 				break;
 			default:
 				kt = 0;
 				break;
+			}
+			if(kt==0) {
+				this.month = Integer.parseInt(date[0]);
+				if(this.month>0&&this.month<13) kt=1;
 			}
 			if(kt == 1) {
 				if(this.month==1||this.month==3||this.month==5||this.month==7||this.month==8||this.month==10||this.month==12) {
@@ -100,7 +104,6 @@ public class MyDate {
 		return kt;
 	}
 	int accept(String st) {
-		if(st.length()<1) return 0;
 		int a;
 		String[] date = new String[3];
 		date = st.split("/");
@@ -108,18 +111,30 @@ public class MyDate {
 		a = checkday(date);
 		return a;
 	}
+	int accept() {
+		LocalDate myObj = LocalDate.now();
+		String[] date = new String[3];
+		String str = String.valueOf(myObj);
+		date = str.split("-");
+		str = date[0];
+		date[0] = date[1];
+		date[1] = date[2];
+		date[2] = str;
+		int i = checkday(date);
+		return i;
+	}
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		MyDate s = new MyDate();
-		LocalDate myObj = LocalDate.now();
 		int a=1;
 		do {
 			if(a==0) System.out.println("Error!");
 			System.out.print("Enter a date (mm/dd/yyyy): ");
-			a = s.accept(input.nextLine());
+			String str = input.nextLine();
+			if(str.length()<1) a = s.accept();
+			else a = s.accept(str);
 		}while(a==0);
-		System.out.println("Day "+s.day+" Month "+s.month+" Year "+s.year);
-		System.out.println(myObj);
+		System.out.println(s.month+"/"+s.day+"/"+s.year);
 		input.close();
 	}
 
